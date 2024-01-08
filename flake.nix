722c95a1a4832a33aaa77ada29dc273f81c14af7
parker <parker.emailaddress@gmail.com>
2024-01-08 00:19:23 -0500

    made host for quadbox

--- flake.nix
+++ flake.nix
@@ -17,10 +17,10 @@
     in
     {
     
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.quadbox = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
-            ./hosts/default/configuration.nix
+            ./hosts/quabox/configuration.nix
             inputs.home-manager.nixosModules.default
           ];
         };
