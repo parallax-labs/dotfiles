23a99f0eccbd58205c7e115ba811c60512bf3ea1
parker <parker.emailaddress@gmail.com>
2024-01-08 00:36:54 -0500

    fixed typo

--- hosts/quadbox/home.nix
+++ hosts/quadbox/home.nix
@@ -82,7 +82,7 @@
       ll = "ls -l";
       update = "sudo nixos-rebuild switch";
     };
-    histSize = 10000;
-    histFile = "${config.xdg.dataHome}/zsh/history";
+    #histSize = 10000;
+    #histFile = "${config.xdg.dataHome}/zsh/history";
   };
 }
